e44901bcde692f353eb5f17298a56d32961142f0
surajrs777 <71411983+surajrs777@users.noreply.github.com>
2021-04-03 20:17:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+from Navigation import home,vaccines,india
+st.set_page_config(layout="wide")
+PAGES = {
+    "Home": home,
+    "Vaccines": vaccines,
+    "India": india
+}
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.test()
